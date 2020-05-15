ed901a145ada67a72f2496d166eba13985699c85
Don <nguyentuand85@gmail.com>
2020-05-14 23:32:58 -0700

    add sort function

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -4,6 +4,12 @@ import "./App.css";
 import Card from "./component/EmployeeCard/Card";
 import Employees from "./component/Employees/Employees.json";
 // import FilterSort from "./component/Buttons/FilterSort";
+import {Dropdown,Table} from 'react-bootstrap';
+import Hero from "./component/Hero";
+import Container from "./component/Container";
+import Row from "./component/Row";
+import Col from "./component/Col";
+
 
 class App extends Component {
   state = {
@@ -18,15 +24,39 @@ class App extends Component {
   render() {
     return (
     <div>
-      <h1 class="title">U.S.S. Enterprise-D Crew</h1>
-      {this.state.Employees.map((Employee) => (
-        <Card
-          key={Employee.id}
-          name={Employee.name}
-          rank={Employee.rank}
-          image={Employee.image}
-        />
-      ))}
+       <Hero backgroundImage="https://i.ytimg.com/vi/EU0xS355BlQ/maxresdefault.jpg">
+       <h1 className="title">U.S.S. Enterprise-D Crew</h1>
+      </Hero>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
+            <Dropdown onSelect={this.handleSelect}>
+                <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                Sort By
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                <Dropdown.Item eventKey="0">ID</Dropdown.Item>
+                <Dropdown.Item eventKey="1">Name</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Rank</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+        </Row>
+        <Row>
+            <Col size="md-12">
+      
+               {this.state.Employees.map((Employee) => (
+              <Card
+              key={Employee.id}
+              name={Employee.name}
+              rank={Employee.rank}
+              image={Employee.image}
+              />
+               ))}
+      
+            </Col>
+         </Row>
+      </Container>
     </div>
     );
   }
